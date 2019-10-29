4802c523fc85e4937d38e21d7d228b675c9fecea
Matt Colson <mc180g@gmail.com>
2019-10-28 22:50:48 -0400

    working on the formula for Minutes Away

--- javascript.js
+++ javascript.js
@@ -9,46 +9,63 @@ var firebaseConfig = {
     storageBucket: "test-project-326f3.appspot.com",
     messagingSenderId: "629232788636",
     appId: "1:629232788636:web:b027a1b1308b87bfa5b959"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var name = "";
+  var place = "";
+  var time = "";
+  var freq = 0;
+
+  
+  
+  $("#submit").on("click", function () {
+      event.preventDefault();
+      
+      name = $("#name-input").val().trim();
+      place = $("#place-input").val().trim();
+      time = $("#time-input").val().trim();
+      freq = $("#freq-input").val().trim();
+
+      + freq
+      
+      minAway = moment([]).toNow(mm);
 
 
-$("#submit").on("click", function () {
-    event.preventDefault();
-
-    name = $("#name-input").val().trim()
-    place = $("#place-input").val().trim()
-    time = $("#time-input").val().trim()
-    freq = $("#freq-input").val().trim()
-
-    database.ref().push({
+      database.ref().push({
 
         name: name,
         place: place,
         time: time,
         freq: freq,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+        
+      })
+
+  })
+
+  database.ref().on("child_added", function(childSnapshot) {
+
+    
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().place);
+    console.log(childSnapshot.val().time);
+    console.log(childSnapshot.val().freq);
+
+    $("#")
 
     })
-
-})
-
-database.ref().on("child_added", function (Snapshot) {
-
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().place);
-    console.log(snapshot.val().freq);
-
-    html(time).snapshot.val()
-})
-minAway = moment("#time-input", "HH:mm")
-
-var newRow = $("<tr>").append(
-    $("<td>").text(name),
-    $("<td>").text(place),
-    $("<td>").text(freq),
-    $("<td>").text(nextArvl),
-    $("<td>").text(minAway),
-
-)
+     database.ref().orderByChild().limitToLast(1).on("child_added", function(snapshot){})
+     var newRow = $("<tr>").append(
+      $("<td>").text(childSnapshot.val().name);
+      $("<td>").text(childSnapshot.val().place);
+      $("<td>").text(childSnapshot.val().freq);
+      $("<td>").text(childSnapshot.val().nextArvl);
+      $("<td>").text(childSnapshot.val().minAway);
+      )
+      
+      minAway = moment("#time-input", "HH:mm")
+      
