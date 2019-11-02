50ed0a0823d7436185a3df85453fa4bcc7dd04ef
Matt Colson <mc180g@gmail.com>
2019-11-01 22:01:57 -0400

    cant get train time figured out

--- javascript.js
+++ javascript.js
@@ -1,71 +1,88 @@
-import { appendFile } from "fs";
-
 // Initialize Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyA7kLrWzteUaPbmuwvmUPyMIW4-xPuwyZ0",
-    authDomain: "test-project-326f3.firebaseapp.com",
-    databaseURL: "https://test-project-326f3.firebaseio.com",
-    projectId: "test-project-326f3",
-    storageBucket: "test-project-326f3.appspot.com",
-    messagingSenderId: "629232788636",
-    appId: "1:629232788636:web:b027a1b1308b87bfa5b959"
+  apiKey: "AIzaSyA7kLrWzteUaPbmuwvmUPyMIW4-xPuwyZ0",
+  authDomain: "test-project-326f3.firebaseapp.com",
+  databaseURL: "https://test-project-326f3.firebaseio.com",
+  projectId: "test-project-326f3",
+  storageBucket: "test-project-326f3.appspot.com",
+  messagingSenderId: "629232788636",
+  appId: "1:629232788636:web:b027a1b1308b87bfa5b959"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+var name = "";
+var place = "";
+var time = "";
+var freq = 0;
+
+
+
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+
+  var name = $("#name-input").val().trim();
+  var place = $("#place-input").val().trim();
+  var time = moment($("#time-input").val().trim(), "HH:mm").format("hh:mma");
+  var freq = moment($("#freq-input").val().trim(), "mm").format("mm");
+
+
+  var newTrain={
+  name: name,
+  place: place,
+  time: time,
+  freq: freq,
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+  database.ref().push(newTrain);
 
-  var name = "";
-  var place = "";
-  var time = "";
-  var freq = 0;
+ 
 
-  
-  
-  $("#submit").on("click", function () {
-      event.preventDefault();
-      
-      name = $("#name-input").val().trim();
-      place = $("#place-input").val().trim();
-      time = $("#time-input").val().trim();
-      freq = $("#freq-input").val().trim();
+  console.log(name);
+  console.log(place);
+  console.log(time);
+  console.log(freq);
 
-      + freq
-      
-      minAway = moment([]).toNow(mm);
+  alert("New Train Added!");
+
+  $("name-input").val("");
+  $("place-input").val("");
+  $("time-input").val("");
+  $("freq-input").val("");
+
+});
 
 
-      database.ref().push({
+database.ref().on("child_added", function (childSnapshot) {
 
-        name: name,
-        place: place,
-        time: time,
-        freq: freq,
-        
-      })
+  var name = childSnapshot.val().name;
+  var place = childSnapshot.val().place;
+  var time = childSnapshot.val().time;
+  var freq = childSnapshot.val().freq;
 
-  })
+  var now = moment();
 
-  database.ref().on("child_added", function(childSnapshot) {
+  var trainTime = moment(time).get('hour' , 'minute');
+  console.log(trainTime);
 
-    
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().place);
-    console.log(childSnapshot.val().time);
-    console.log(childSnapshot.val().freq);
+  var nextArvl = moment(trainTime).set('hour', 'minute').add(freq, 'minutes');
+  console.log(nextArvl);
 
-    $("#")
+  var minAway =moment.duration(now.to(nextArvl)).asMinutes();
 
-    })
-     database.ref().orderByChild().limitToLast(1).on("child_added", function(snapshot){})
-     var newRow = $("<tr>").append(
-      $("<td>").text(childSnapshot.val().name);
-      $("<td>").text(childSnapshot.val().place);
-      $("<td>").text(childSnapshot.val().freq);
-      $("<td>").text(childSnapshot.val().nextArvl);
-      $("<td>").text(childSnapshot.val().minAway);
-      )
-      
-      minAway = moment("#time-input", "HH:mm")
-      
+   
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(name),
+    $("<td>").text(place),
+    $("<td>").text(freq),
+    $("<td>").text(nextArvl),
+    $("<td>").text(minAway)
+  );
+
+  $("#train-sched > tbody").append(newRow);
+
+
+});
